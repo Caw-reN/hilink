3f794db174cff5c149821dcdad03366c04eedd3f
Caw-reN <hibrizitsaqif30@gmail.com>
2026-01-23 23:12:24 +0700

    Configure User and Link models with fillable and relationships

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Link extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'title',
+        'url',
+        'is_active',
+        'position',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'username',
+        'bio',
+        'avatar',
+        'bg_color',
     ];
 
     /**
@@ -44,5 +48,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function links()
+    {
+        return $this->hasMany(Link::class);
     }
 }
