fa89cb644690086987812a366de22b972c25c81c
Caw-reN <hibrizitsaqif30@gmail.com>
2026-01-25 20:16:31 +0700

    Create landing page for HiLink V1

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,7 +11,11 @@
         <!-- Username -->
         <div class="mt-4">
             <x-input-label for="username" :value="__('Username')" />
-            <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autocomplete="username" />
+            <x-text-input id="username" class="block mt-1 w-full" 
+                  type="text" 
+                  name="username" 
+                  :value="request('username') ?? old('username')" 
+                  required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('username')" class="mt-2" />
         </div>
 
